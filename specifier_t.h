9e3f36873d750205a2972319ef4dc06dcf62cec8
Frederic Bourouliou <bourouliou@gmail.com>
2024-11-27 15:01:39 +0100

    Add specifier_t.h file

--- specifier_t.h
+++ specifier_t.h
@@ -1,10 +1,13 @@
 #ifndef SPECIFIER_T_H
 #define SPECIFIER_T_H
-
+/**
+ * struct specifier_t - data structur
+ * @specifier: a variable
+ * @print_function: a pointer
+ */
 typedef struct specifier_t
 {
-        char specifier;
-        int (*print_function)(va_list);
+char specifier;
+int (*print_function)(va_list);
 } specifier_t;
-
 #endif
